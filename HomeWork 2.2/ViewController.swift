b3e1869291be360faf0753b151c64a1ba1052b3d
DanilaBratinov <bratinov0704@inbox.ru>
2022-10-22 19:19:00 +0300

    Created a traffic light project: Creating UI elements Working with constraints Writing project logic Working with animation

--- HomeWork 2.2/ViewController.swift
+++ HomeWork 2.2/ViewController.swift
@@ -1,19 +1,61 @@
-//
-//  ViewController.swift
-//  HomeWork 2.2
-//
-//  Created by Данила Братинов on 20.10.2022.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var redColorView: UIView!
+    @IBOutlet var yellowColorView: UIView!
+    @IBOutlet var greenColorView: UIView!
+    
+    @IBOutlet var buttonOutlet: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redColorView.layer.cornerRadius = 55
+        redColorView.alpha = 0.5
+        
+        yellowColorView.layer.cornerRadius = 55
+        yellowColorView.alpha = 0.5
+        
+        greenColorView.layer.cornerRadius = 55
+        greenColorView.alpha = 0.5
+        
+        buttonOutlet.setTitle("START", for: .normal)
+        buttonOutlet.layer.cornerRadius = 15
     }
-
-
+    
+    var tapCount = 0
+    @IBAction func changeColorButton() {
+        tapCount += 1
+        changeColor()
+    }
+    
+    private func changeColor() {
+        switch tapCount {
+        case 1:
+            UIView.animate(withDuration: 0.3) {
+                self.redColorView.alpha = 1
+                self.buttonOutlet.setTitle("NEXT", for: .normal)
+            }
+        case 2:
+            UIView.animate(withDuration: 0.3) {
+                self.redColorView.alpha = 0.5
+                self.yellowColorView.alpha = 1
+            }
+        case 3:
+            UIView.animate(withDuration: 0.3) {
+                self.yellowColorView.alpha = 0.5
+                self.greenColorView.alpha = 1
+            }
+        case 4:
+            UIView.animate(withDuration: 0.3) {
+                self.redColorView.alpha = 0.5
+                self.yellowColorView.alpha = 0.5
+                self.greenColorView.alpha = 0.5
+                self.buttonOutlet.setTitle("START", for: .normal)
+            }
+            tapCount = 0
+        default:
+            break
+        }
+    }
 }
 
